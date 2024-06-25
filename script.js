4f7c6f6879694e6d82ad0cec5f7d47191ca5ce46
VishnuKumar006 <JaiShreeRam06@hotmail.com>
2024-06-25 14:27:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var typed = new Typed('#element', {
+    strings: ['Welcome to StudySync !', 'Here I will You basic to advanced', 'Coding technique which took me ages to learn', 'You can ask your doubts with our paid community also.', 'here I will teach you Web &amp; App Development', '&amp; some Programming Languages like Html', 'Css, Javascript, C++, C languages, Java, Python', '&amp; DSA (Data Structure &amp; Algorithms)', 'with that courses are free for starting *100*', 'Users so go now &amp; login your account on StudySync !'],
+    typeSpeed: 50,
+    backspeed: 50,
+    loop: true
+});
+
+setTimeout(function() {
+    alert("Accept all Cookies")
+}, 12000);
+
+setTimeout(function() {
+    alert("Crack the Code To success with the StudySync,")
+}, 3000);
+
+var typed = new Typed('#Element', {
+    strings: ['transformative journey to simplify education and unlock your full potential.'],
+    typeSpeed: 100,
+    backspeed: 60,
+    loop: true
+});
+
+var typed = new Typed('#sync', {
+    strings: ['Sync'],
+    typeSpeed: 150,
+    loop: true
+});
+
+var typed = new Typed('.element', {
+    strings: ['Brighter Futures,'],
+    typeSpeed: 120,
+    backspeed: 60,
+    loop: true
+});
